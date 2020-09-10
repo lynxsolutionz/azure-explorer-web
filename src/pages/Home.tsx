d61a807d15e064a66706d0f0265e8b186caaced5
mudassar045 <mudassaralics007@gmail.com>
2020-09-11 01:11:08 +0500

    initial setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from 'layout/Layout'
+
+type S = {
+
+}
+
+const Home = () => {
+
+	return <Layout>
+		<div className="section hero-body">
+		</div>
+	</Layout>
+}
+
+export default Home
