4f7ba4bb4d7aa7d209e11af515211bb17cfee6e0
mudassar045 <mudassaralics007@gmail.com>
2020-09-12 01:40:25 +0500

    Updates

---
+++ precache-manifest.553351ed5afd87cfb3b5a75615c6fe55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605ddc2fefaea32b7d1a3e50d918e83e",
+    "url": "/azure-explorer-web/index.html"
+  },
+  {
+    "revision": "da6c439746e9837be665",
+    "url": "/azure-explorer-web/static/css/main.b48cb2ac.chunk.css"
+  },
+  {
+    "revision": "6b8ee0391094cf91ee53",
+    "url": "/azure-explorer-web/static/js/2.7a2c9c67.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/azure-explorer-web/static/js/2.7a2c9c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da6c439746e9837be665",
+    "url": "/azure-explorer-web/static/js/main.533a5f24.chunk.js"
+  },
+  {
+    "revision": "e61fda5d890aac2f38da",
+    "url": "/azure-explorer-web/static/js/runtime-main.227ea1f7.js"
+  },
+  {
+    "revision": "f07e5eaba43268cb5a4d40eb18d84b7c",
+    "url": "/azure-explorer-web/static/media/machine-learning.f07e5eab.svg"
+  }
+]);
